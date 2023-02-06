dd0ba12045fb5f5d35aaec21ca4cb76b74f531ae
RileyGrant-lough <riley@3grants.net>
2023-02-06 15:00:36 +0000

    Server code

--- linuxEthClient.cpp
+++ linuxEthClient.cpp
@@ -17,13 +17,13 @@ int main(int argc, char const* argv[])
 		printf("\n Socket creation error \n");
 		return -1;
 	}
-	
+
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(PORT);
 
 	// Convert IPv4 and IPv6 addresses from text to binary
 	// form
-	if (inet_pton(AF_INET, "192.168.0.75", &serv_addr.sin_addr) //Server IP in this line
+	if (inet_pton(AF_INET, "169.254.49.240", &serv_addr.sin_addr)
 		<= 0) {
 		printf(
 			"\nInvalid address/ Address not supported \n");
