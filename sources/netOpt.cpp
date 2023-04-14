b2fb1fab132c42b384b82b31ee256ead78bd1142
RileyGrant-lough <riley@3grants.net>
2023-04-14 17:10:03 +0100

    adjusting weighting

--- sources/netOpt.cpp
+++ sources/netOpt.cpp
@@ -1387,9 +1387,9 @@ int8_t netOpt::light2recordPlayer(roomMember *light, roomMember *recordPlayer)
                 {
                     if(a2->timestamp > a3->timestamp)
                     {
-                        if(probChange <= 77)
+                        if(probChange <= 117)
                         {
-                            probChange = probChange + 50;
+                            probChange = probChange + 10;
                         }
                         else
                         {
