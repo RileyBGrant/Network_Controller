0ea66e5f57d3dfe5cf3ab80bebb69768bf56ecd8
RileyGrant-lough <riley@3grants.net>
2023-04-23 18:09:51 +0100

    removing uneeded records

--- sources/netOpt.cpp
+++ sources/netOpt.cpp
@@ -1978,7 +1978,7 @@ int netOpt::characteriseUsage()
                             d1->activity.remove(counterA1);
                         }
                     }
-                    else if(a1->variable == 0 && a1->state == 1 && counterA1 == 0)
+                    else if(a1->variable == 0 && a1->state != 2 && counterA1 == 0)
                     {
                         d1->activity.remove(counterA1);
                     }
